10a2a41ea792c567d43821610aceccab3648e997
JuniorMalinga <ST10438896@Vcconnect.edu.za>
2026-04-16 22:33:03 +0200

    updated the project to "work" more function has to be added by the team NOTE: there might be some visual errros those will be ironed out just make it work for now (ST10438896)

--- app/src/main/java/com/spendid/app/MainActivity.kt
+++ app/src/main/java/com/spendid/app/MainActivity.kt
@@ -5,32 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.navHostFragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        
-        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigation)
-        bottomNav.setupWithNavController(navController)
-        
-        findViewById<FloatingActionButton>(R.id.fabAddExpense).setOnClickListener {
-            navController.navigate(R.id.addExpenseFragment)
-        }
+        setContentView(R.layout.activity_splash)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.navHostFragment)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
         }
     }
-}
